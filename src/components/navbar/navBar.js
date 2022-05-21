08a129d93125e6c8e693647db2d4baf9b9549d81
Ludho <Ludho234@gmail.com>
2022-05-21 17:07:50 +0200

    :art: css navbar

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-
+import { Navbar, Nav,Container } from 'react-bootstrap';
+import './navBar.css'
 
 const AppNavBar = () => {
   return (
 
       <Navbar style={{backgroundColor:"red",marginBottom:"20px"}}>
+        <Container>
 
+        <Navbar.Brand className='text-white' href="/">Home</Navbar.Brand>
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-              <Nav.Link className='text-white' href="/">Home</Nav.Link>
-                <Nav.Link className='text-white' href="/pokemons">Pokédex</Nav.Link>
+              <Nav className="w-100 justify-content-center">
+                <span><Nav.Link className='text-white pokeball' href="/pokemons"><img style={{width:"50px",height:"50px"}} src={process.env.REACT_APP_LOCALAPI+"/pokeball.png"}></img></Nav.Link></span>
+                
+              </Nav>
+              <Nav>
                 <Nav.Link className='text-white' href="/teams">Teams</Nav.Link>
               </Nav>
             </Navbar.Collapse>
+
+        </Container>
 
       </Navbar>
 
